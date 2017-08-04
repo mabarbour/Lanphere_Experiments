52c439a2a6b78ad2926851e3e81d528c342f8bbe
mabarbour <matthew.a.barbour@gmail.com>
2017-08-04 11:36:44 +0000

    script for loading libraries and functions for all analyses.

--- scripts_for_analysis/required_libraries.R
+++ scripts_for_analysis/required_libraries.R
@@ -1,59 +1,28 @@
 
+## LIBRARIES ----
 
-#source('~/Documents/miscellaneous_R/model_diagnostic_functions.R')
-library(merTools) # must be loaded before dplyr
-library(dplyr)
-library(tidyr)
-#library(reshape2)
-#library(ggplot2)
-library(lme4)
+library(merTools) # must be loaded before dplyr, otherwise 'select()' function is masked
+
+library(tidyverse) # easy dataframe manipulation
+
+library(lme4) # analyzing mixed-effect models
+
 library(effects) # calculating mean and confidence intervals of treatment and genotype effects.
-library(psych) # for correlation tests
-#library(car) # for Anova function
-#library(broom) # for tidying up model outputs
-library(RCurl)
-script <- getURL("https://raw.githubusercontent.com/mabarbour/miscellaneous_R/master/model_diagnostic_functions.R", ssl.verifypeer = FALSE)
-eval(parse(text = script))
 
-#source('~/Documents/miscellaneous_R/model_diagnostic_functions.R')
-library(merTools) # must load before dplyr since this package requires 'MASS' which requires 'plyr'
-library(dplyr)
-library(tidyr)
-library(reshape2)
-library(ggplot2)
-library(psych)
-library(lme4)
-library(car)
-library(vegan)
-library(effects)
-library(ggplot2)
-library(cowplot)
+library(psych) # correlation tests
 
-library(RCurl) # for loading github files directly.
+library(vegan) # analyzing community data
 
-script <- getURL("https://raw.githubusercontent.com/mabarbour/miscellaneous_R/master/autoplot.custom.R", ssl.verifypeer = FALSE)
-eval(parse(text = script))
+library(cowplot) # souped up version of ggplot
 
-script <- getURL("https://raw.githubusercontent.com/mabarbour/miscellaneous_R/master/veganCovEllipse.R", ssl.verifypeer = FALSE)
-eval(parse(text = script))
+library(RCurl) # for pulling code from other github repos
 
-library(RCurl)
-script <- getURL("https://raw.githubusercontent.com/mabarbour/miscellaneous_R/master/model_diagnostic_functions.R", ssl.verifypeer = FALSE)
-eval(parse(text = script))
-script <- getURL("https://raw.githubusercontent.com/mabarbour/miscellaneous_R/master/veganCovEllipse.R", ssl.verifypeer = FALSE)
-eval(parse(text = script))
-#script <- getURL("https://raw.githubusercontent.com/mabarbour/miscellaneous_R/master/beta.div.R", ssl.verifypeer = FALSE)
-#eval(parse(text = script))
-#source('~/Documents/miscellaneous_R/model_diagnostic_functions.R')
-#source('~/Documents/miscellaneous_R/veganCovEllipse.R')
-#source('~/Documents/miscellaneous_R/beta.div.R')
-library(merTools) # must load before dplyr since this package requires 'MASS' which requires 'plyr'
-library(effects)
-library(dplyr)
-library(tidyr)
-library(vegan)
-library(lme4)
-library(ggplot2)
-library(cowplot) # throwing an error for unknown reason
+## FUNCTIONS ----
+
+eval(parse(text = getURL("https://raw.githubusercontent.com/mabarbour/miscellaneous_R/master/model_diagnostic_functions.R", ssl.verifypeer = FALSE)))
+
+eval(parse(text = getURL("https://raw.githubusercontent.com/mabarbour/miscellaneous_R/master/autoplot.custom.R", ssl.verifypeer = FALSE)))
+
+eval(parse(text = getURL("https://raw.githubusercontent.com/mabarbour/miscellaneous_R/master/veganCovEllipse.R", ssl.verifypeer = FALSE)))
 
 
