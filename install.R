9306d4c5c5e66120560e8bd201188969b9756c8d
mabarbour <matthew.a.barbour@gmail.com>
2018-02-27 16:50:02 +0100

    add preliminary files to run R + Jupyter

---
+++ install.R
@@ -0,0 +1,7 @@
+install.packages("tidyverse")
+install.packages("brms")
+install.packages("rstan")
+install.packages("broom")
+install.packages("parallel")
+install.packages("cowplot")
+install.packages("stringr")
