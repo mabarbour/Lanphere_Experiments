40bb7f0efaa52553c5daf29ed80b407c80b15b9e
mabarbour <matthew.a.barbour@gmail.com>
2018-05-10 13:59:12 +0200

    modify to try and get binder to work

--- install.R
+++ install.R
@@ -1,9 +1,9 @@
 install.packages("tidyverse")
+install.packages("rmarkdown")
 install.packages("brms")
 install.packages("rstan")
 install.packages("broom")
 install.packages("parallel")
 install.packages("cowplot")
 install.packages("stringr")
-install.packages("rmarkdown")
-#install.packages("installr")
+
